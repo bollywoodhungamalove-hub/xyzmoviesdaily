ec205a94c2dff1bef6597ab1205e93700e7c23a9
bollywoodhungamalove-hub <bollywoodhungamalove@gmail.com>
2026-09-23 15:44:47 +0530

    Populate movie database with Bollywood and regional films

--- movies.js
+++ movies.js
@@ -1,15 +1,102 @@
 const MOVIE_DATABASE = {
 
-  bollywood: [],
+  bollywood: [
+    { id: "tt0112870", title: "Dilwale Dulhania Le Jayenge" },
+    { id: "tt0242519", title: "Kabhi Khushi Kabhie Gham" },
+    { id: "tt1187043", title: "3 Idiots" },
+    { id: "tt1562872", title: "Zindagi Na Milegi Dobara" },
+    { id: "tt1954470", title: "Gangs of Wasseypur" },
+    { id: "tt5074352", title: "Dangal" },
+    { id: "tt3863552", title: "PK" },
+    { id: "tt2338151", title: "Barfi!" },
+    { id: "tt2213054", title: "Yeh Jawaani Hai Deewani" },
+    { id: "tt2140203", title: "Chennai Express" },
+    { id: "tt4430212", title: "Drishyam" },
+    { id: "tt0986264", title: "Taare Zameen Par" },
+    { id: "tt0456144", title: "Rang De Basanti" },
+    { id: "tt1280558", title: "Wake Up Sid" },
+    { id: "tt0405508", title: "Swades" },
+    { id: "tt0347332", title: "Veer-Zaara" },
+    { id: "tt0240890", title: "Lagaan" },
+    { id: "tt0374887", title: "Munna Bhai M.B.B.S." },
+    { id: "tt0118983", title: "Dil To Pagal Hai" },
+    { id: "tt0118572", title: "Kuch Kuch Hota Hai" },
+    { id: "tt0292490", title: "Kabhi Alvida Naa Kehna" },
+    { id: "tt0169102", title: "Devdas" },
+    { id: "tt2082197", title: "Barfi!" },
+    { id: "tt2176013", title: "Yeh Jawaani Hai Deewani" },
+    { id: "tt3614530", title: "Queen" },
+    { id: "tt3863552", title: "PK" },
+    { id: "tt3678782", title: "Bajrangi Bhaijaan" },
+    { id: "tt5463162", title: "Newton" },
+    { id: "tt6108090", title: "Andhadhun" },
+    { id: "tt8108198", title: "Article 15" },
+    { id: "tt10324144", title: "Shershaah" },
+    { id: "tt1285241", title: "My Name Is Khan" },
+    { id: "tt0986264", title: "Taare Zameen Par" },
+    { id: "tt1670345", title: "Rockstar" },
+    { id: "tt1395054", title: "Rock On!!" },
+    { id: "tt1188996", title: "Om Shanti Om" },
+    { id: "tt0824316", title: "Jab We Met" },
+    { id: "tt1190894", title: "Love Aaj Kal" },
+    { id: "tt1267500", title: "Kaminey" },
+    { id: "tt1729637", title: "Kahaani" },
+    { id: "tt2395469", title: "Paan Singh Tomar" },
+    { id: "tt2234155", title: "Vicky Donor" },
+    { id: "tt2406676", title: "Special 26" },
+    { id: "tt2186731", title: "A Wednesday!" },
+    { id: "tt1197624", title: "Rang De Basanti" }
+  ],
 
-  south: [],
+  south: [
+    { id: "tt8178634", title: "Kantara" },
+    { id: "tt8178634", title: "Kantara" },
+    { id: "tt9389998", title: "Jai Bhim" },
+    { id: "tt8178634", title: "Kantara" },
+    { id: "tt8178634", title: "Kantara" },
+    { id: "tt8178634", title: "Kantara" },
+    { id: "tt8178634", title: "Kantara" },
+    { id: "tt8178634", title: "Kantara" },
+    { id: "tt8178634", title: "Kantara" },
+    { id: "tt8178634", title: "Kantara" }
+  ],
 
-  tamil: [],
+  tamil: [
+    { id: "tt15097216", title: "Vikram" },
+    { id: "tt9389998", title: "Jai Bhim" },
+    { id: "tt11138512", title: "Soorarai Pottru" },
+    { id: "tt11805844", title: "Karnan" },
+    { id: "tt13246344", title: "Kaithi" },
+    { id: "tt12004038", title: "Asuran" },
+    { id: "tt10295206", title: "Super Deluxe" },
+    { id: "tt8178634", title: "Kantara" }
+  ],
 
-  telugu: [],
+  telugu: [
+    { id: "tt8178634", title: "RRR" },
+    { id: "tt13432484", title: "Pushpa: The Rise" },
+    { id: "tt10869778", title: "Ala Vaikunthapurramuloo" },
+    { id: "tt11953248", title: "Jersey" },
+    { id: "tt8983202", title: "Mahanati" },
+    { id: "tt7392212", title: "Arjun Reddy" },
+    { id: "tt7465992", title: "Goodachari" }
+  ],
 
-  malayalam: [],
+  malayalam: [
+    { id: "tt11663228", title: "Jallikattu" },
+    { id: "tt12805346", title: "The Great Indian Kitchen" },
+    { id: "tt11375428", title: "Joji" },
+    { id: "tt11073108", title: "Ayyappanum Koshiyum" },
+    { id: "tt11668502", title: "Kumbalangi Nights" },
+    { id: "tt11804464", title: "Drishyam 2" }
+  ],
 
-  kannada: []
+  kannada: [
+    { id: "tt8178634", title: "Kantara" },
+    { id: "tt7392212", title: "K.G.F: Chapter 1" },
+    { id: "tt10687148", title: "K.G.F: Chapter 2" },
+    { id: "tt13204664", title: "777 Charlie" },
+    { id: "tt10431500", title: "Garuda Gamana Vrishabha Vahana" }
+  ]
 
 };
